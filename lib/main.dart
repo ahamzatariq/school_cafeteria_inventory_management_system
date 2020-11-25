29a14fcbe292d6aa5ead52778348888e1317529f
Ameer Hamza Tariq <30323486+ahamzatariq@users.noreply.github.com>
2020-11-25 14:10:37 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    List _isHovering = [false, false, false, false];
+
+    return Scaffold(
+      body: SliderMenuContainer(
+        sliderMenu: LayoutBuilder(
+          builder: (context, constraints) => Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 16,
+            ),
+            child: ListView(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.face,
+                        size: constraints.maxWidth / 3.0,
+                      ),
+                      maxRadius: constraints.maxWidth / 5.0,
+                    ),
+                    SizedBox(width: 7),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Role',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        sliderMain: FlutterLogin(
+          title: 'Canteen Inventory System',
+          logo: 'assets/food_logo.png',
+          onLogin: _authUser,
+          onSignup: _authUser,
+          onRecoverPassword: (_) {
+            return null;
+          },
+          onSubmitAnimationCompleted: () {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => Container(),
+              ),
+            );
+          },
+          showDebugButtons: true,
+        ),
+      ),
+    );
+  }
+
+  Future<String> _authUser(LoginData p1) {}
+}
