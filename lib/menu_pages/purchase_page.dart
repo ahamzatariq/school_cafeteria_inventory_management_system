af277ad3d878c149f8efb00234651b336b90ff29
Ameer Hamza Tariq <30323486+ahamzatariq@users.noreply.github.com>
2020-12-02 12:40:29 +0500

    Purchase form designed.

--- lib/menu_pages/purchase_page.dart
+++ lib/menu_pages/purchase_page.dart
@@ -1,9 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/style.dart';
 
-class PurchasePage extends StatelessWidget{
+class PurchasePage extends StatelessWidget {
   static const routeName = '/purchase-page';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Form(
+        child: Row(
+          children: [
+            Expanded(flex: 2, child: Spacer()),
+            Expanded(
+              flex: 1,
+              child: Card(
+                elevation: 6,
+                color: Theme.of(context).primaryColorLight.withAlpha(70),
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Purchase Items',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        style: Theme.of(context).textTheme.headline3,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.widgets_outlined),
+                          labelText: 'Item Name',
+                        ),
+                      ),
+                      TextFormField(
+                        style: Theme.of(context).textTheme.headline3,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.arrow_back_rounded),
+                          labelText: 'Purchasing Amount',
+                        ),
+                      ),
+                      TextFormField(
+                        style: Theme.of(context).textTheme.headline3,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.add_shopping_cart_rounded),
+                          labelText: 'Quantity Purchased',
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 40.0),
+                            child: Text(
+                              'Per piece cost: ',
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                          ),
+                          Text(
+                            '15',
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        style: Theme.of(context).textTheme.headline3,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.arrow_forward_rounded),
+                          labelText: 'Selling Price',
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        child: Text(
+                          'Purchase',
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(flex: 2, child: Spacer()),
+          ],
+        ),
+      ),
+    );
   }
 }
